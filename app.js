da064ae4634c827580c8e40094260d3cad87f6dd
Your Name <shivanshchangle1@gmail.com>
2018-09-06 21:38:20 +0530

    changes for heroku

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var appcontroller = require('./controller/appcontroller');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 appcontroller(app);
 
-app.listen(3000);
+app.listen(port);
 console.log('listening on port 3000');
